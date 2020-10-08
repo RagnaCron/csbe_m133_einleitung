70874bc8e4ef3ecca28869fe5c2742d8f9256028
Manuel Werder <manuel.werder@ict.csbe.ch>
2020-10-08 19:53:53 +0200

    Added a Schedule

--- create-docs.sh
+++ create-docs.sh
@@ -6,9 +6,9 @@ then
 pwd
 fi
 
-#xelatex -output-directory=./build ./main-manual.tex
-#biber --output-directory ./build ./main-manual
-#xelatex -output-directory=./build ./main-manual.tex
+xelatex -output-directory=./build ./main-manual.tex
+biber --output-directory ./build ./main-manual
+xelatex -output-directory=./build ./main-manual.tex
 
 xelatex -output-directory=./build ./main-schedule.tex
 biber --output-directory ./build ./main-schedule
